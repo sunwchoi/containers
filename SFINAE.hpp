a87e66ff3e6d23f1c5d01966f3178dda8b736b00
sunwchoi <chltjsdn103@naver.com>
2023-03-15 10:49:26 +0900

    Feat: is_integral 수정
    1. wchar_t, char_t 등 fundamental integral type 추가

--- SFINAE.hpp
+++ SFINAE.hpp
@@ -21,6 +21,9 @@ template <class T> struct is_integral: public false_type{};
 
 template <> struct is_integral<bool>: public true_type{};
 template <> struct is_integral<char>: public true_type{};
+template <> struct is_integral<signed char>: public true_type{};
+template <> struct is_integral<wchar_t>: public true_type{};
+template <> struct is_integral<char16_t>: public true_type{};
 template <> struct is_integral<short>: public true_type{};
 template <> struct is_integral<int>: public true_type{};
 template <> struct is_integral<long>: public true_type{};
